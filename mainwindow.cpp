bf993cef16d4fcd36b0c65d5cafead7be7bb8d6c
iegrsy <iegursoy28@gmail.com>
2017-08-09 17:55:54 +0300

    first day

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,98 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+
+
+    QString jfilepath = "/home/ieg/qt/qtProjects/ConfigurationSetting/settings.json";
+    QString jstr = readFile(jfilepath);
+    readJson(jstr);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+QString MainWindow::readFile(QString path)
+{
+    QFile file(path);
+    if(!file.open(QIODevice::ReadOnly))
+        return 0;
+
+    QString jdata = file.readAll().data();
+    file.close();
+
+    return jdata;
+}
+
+void MainWindow::readJson(QString str)
+{
+    if(str.isEmpty())
+        return;
+
+    jDoc = QJsonDocument::fromJson(str.toUtf8());
+    QJsonObject jObj = jDoc.object();
+    ui->lstMenu->addItems(jObjToListMenu(jObj));
+
+}
+
+QStringList MainWindow::jObjToListMenu(QJsonObject jObj)
+{
+    QStringList itemList;
+
+    foreach (QString s, jObj.keys())
+    {
+        itemList.append(s);
+
+        QJsonObject jo = jObj[s].toObject();
+        QJsonArray jAr;
+        foreach (QString s, jo.keys()) {
+            if(s.at(0) == '_' && jo[s].isArray())
+            {
+                jAr = jo[s].toArray();
+
+                for(int i = 0; i < jAr.count(); i++)
+                {
+                    QJsonObject o = jAr[i].toObject();
+                    itemList.append(o["name"].toString());
+                }
+            }
+        }
+    }
+    return itemList;
+}
+
+
+
+
+void MainWindow::on_lstMenu_itemDoubleClicked(QListWidgetItem *item)
+{
+    qDebug()<<item->text();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,18 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
+#include <QFile>
+#include <QDebug>
+#include <QStringList>
+#include <QListWidgetItem>
+
+#include <QJsonParseError>
+#include <QDragEnterEvent>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonValue>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +27,17 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+
+    void on_lstMenu_itemDoubleClicked(QListWidgetItem *item);
+
 private:
     Ui::MainWindow *ui;
+    QJsonDocument jDoc;
+
+    QString readFile(QString path);
+    void readJson(QString str);
+    QStringList jObjToListMenu(QJsonObject jObj);
 };
 
 #endif // MAINWINDOW_H
